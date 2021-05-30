df71757790702da0c176b32b1fe9e5f08c579bc6
Nabajyoti <sirnaba@gmail.com>
2021-05-30 18:59:42 +0530

    Cart icon adn card button component added in header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,12 +2,15 @@ import React, { Fragment } from "react";
 import classes from "./Header.module.css";
 import meals from "../../assets/meals.jpg";
 
+//Componets
+import HeaderCartButton from "./HeaderCartButton";
+
 function Header() {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>React Meals</h1>
-        <button>Cart</button>
+        <HeaderCartButton></HeaderCartButton>
       </header>
       <div className={classes["main-image"]}>
         <img src={meals} alt="Table of meals"></img>
