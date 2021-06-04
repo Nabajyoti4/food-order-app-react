78cbc2a1a23652d3ce3468d419d7e69f779a856c
Nabajyoti <sirnaba@gmail.com>
2021-06-04 10:47:32 +0530

    Error message handling added if meals fetched fail

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -8,31 +8,56 @@ import MealItem from "./MealItem/MealItem";
 
 function AvailableMeals() {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
 
   useEffect(() => {
     const fetchMeals = async () => {
-      const response = await fetch(
-        "https://food-order-react-app-default-rtdb.firebaseio.com/meals.json"
-      );
+      try {
+        const response = await fetch(
+          "https://food-order-react-app-default-rtdb.firebaseio.com/meals.json"
+        );
 
-      const data = await response.json();
+        const data = await response.json();
 
-      const loadMeals = [];
+        console.log(data);
 
-      for (const key in data) {
-        loadMeals.push({
-          id: key,
-          name: data[key].name,
-          description: data[key].description,
-          price: data[key].price,
-        });
+        const loadMeals = [];
+
+        for (const key in data) {
+          loadMeals.push({
+            id: key,
+            name: data[key].name,
+            description: data[key].description,
+            price: data[key].price,
+          });
+          setMeals(loadMeals);
+        }
+      } catch (err) {
+        setHttpError(err.message);
       }
 
-      setMeals(loadMeals);
+      setIsLoading(false);
     };
 
     fetchMeals();
   }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.meals__loading}>
+        <p>Loading Meals ...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.meals__error}>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
 
   const mealsList = meals.map((meal) => {
     return (
